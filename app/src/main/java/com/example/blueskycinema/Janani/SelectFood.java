27b2708ce5d499a1e82a56571b5e1885c4845db5
Janani Malshika <it20002824@my.sliit.lk>
2021-09-19 15:54:55 +0530

    Java Classes Created

--- app/src/main/java/com/example/blueskycinema/Janani/SelectFood.java
+++ app/src/main/java/com/example/blueskycinema/Janani/SelectFood.java
@@ -6,11 +6,17 @@ import android.os.Bundle;
 
 import com.example.blueskycinema.R;
 
-public class my_cart extends AppCompatActivity {
+public class SelectFood extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my_cart);
+        setContentView(R.layout.activity_select_food);
     }
 }
+
+
+
+
+
+
