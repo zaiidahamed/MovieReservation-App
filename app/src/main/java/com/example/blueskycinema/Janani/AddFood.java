d21c2f2311eb381d20e70d779ba6b2a0c3351bcf
Janani Malshika <it20002824@my.sliit.lk>
2021-09-21 15:18:56 +0530

    Update AddFood.java

--- app/src/main/java/com/example/blueskycinema/Janani/AddFood.java
+++ app/src/main/java/com/example/blueskycinema/Janani/AddFood.java
@@ -1,17 +1,19 @@
 package com.example.blueskycinema.Janani;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-
+import com.example.blueskycinema.DB_Handler;
 import com.example.blueskycinema.R;
 
 public class AddFood extends AppCompatActivity {
+
+    DB_Handler myDB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_food);
+        myDB = new DB_Handler(this);
     }
 }
 
