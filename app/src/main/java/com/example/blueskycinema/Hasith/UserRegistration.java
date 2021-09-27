de1ad817628b0ea276e06ca4e5ff0255aaff64b0
H.A. Hasith Deminda <it20238612@my.sliit.lk>
2021-09-27 23:42:45 +0530

    Movie details page for users created!

---
+++ app/src/main/java/com/example/blueskycinema/Hasith/UserRegistration.java
@@ -0,0 +1,19 @@
+package com.example.blueskycinema.Hasith;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.blueskycinema.R;
+
+public class UserRegistration extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.signup);
+
+    }
+}
