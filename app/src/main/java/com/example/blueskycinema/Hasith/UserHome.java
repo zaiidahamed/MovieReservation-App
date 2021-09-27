94007a5d013e090a44ab811be8269252d0e6aa6e
H.A. Hasith Deminda <it20238612@my.sliit.lk>
2021-09-27 23:41:50 +0530

    User Home created!

---
+++ app/src/main/java/com/example/blueskycinema/Hasith/UserHome.java
@@ -0,0 +1,44 @@
+package com.example.blueskycinema.Hasith;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.example.blueskycinema.DB_Handler;
+import com.example.blueskycinema.R;
+
+public class UserHome extends AppCompatActivity {
+
+    RecyclerView mRecyclerView;
+    DB_Handler databaseHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_home);
+
+        mRecyclerView = findViewById(R.id.recyclerView);
+        databaseHelper = new DB_Handler(this);
+
+        mRecyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+
+
+        showRecord();
+
+    }
+
+    private void showRecord() {
+        UserAdapter adapter = new UserAdapter(UserHome.this, databaseHelper.getAllData(DB_Handler.MOVIE_COLUMN_START_DATE+ " DESC"));
+
+        mRecyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        showRecord();
+    }
+
+}
